7177a63c01f958bf637495a3154b0dbae7e7f7f3
2pac-icecyb <gela6ix9ine@gmail.com>
2025-07-13 16:24:55 +0400

    day 01 homework
    day 01 homework

--- variables.py
+++ variables.py
@@ -1,9 +1,5 @@
-first_name = "Gela"
-last_name = "mchedlishvili"
-height = 1.78
-gender = "male"
+name = "tata"
+name2 = "gela"
 
-print(first_name)
-print(last_name)
-print(height)
-print(gender)
+print(name)
+print(name2)
